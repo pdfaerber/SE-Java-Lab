190265255d35d8fe25ff847bc5c439dfc9ab8c51
pdfaerber <87340230+pdfaerber@users.noreply.github.com>
2021-10-05 10:02:25 -0400

    Module 3, Week 1, java array lab assignment

---
+++ ArrayLab.java
@@ -0,0 +1,22 @@
+/**************************************************************************************
+ * Patrick D. Faerber, Per Scholas, SE Cohort 13, October 5, 2021
+ * Mod3: Week 1, Day 3 Lab, Arrays
+ * 
+ * Arrays practice:
+ * 1) Display the average of a given integer array
+ * 2) Display the number of times a user entered string is found in the given string array.
+ * 3) Copy one array into another (without any built-in method) and display the new array
+ *   
+ ***************************************************************************************/
+package arrayLab;
+import java.util.Scanner;
+
+public class ArrayLab {
+
+	public static void main(String[] args) {
+		
+		
+
+	}//end main
+
+}//end ArrayLab
